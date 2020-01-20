8c06c63d9c61931caee6e3a5bc7fb9ae8b767b23
serfin <gracjanbrylka@gmail.com>
2020-01-20 23:58:03 +0100

    Added migrations for customer context

---
+++ src/BeComfy.Services.Customers.Infrastructure/Migrations/20200120215753_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BeComfy.Services.Customers.Infrastructure.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(nullable: false),
+                    FirstName = table.Column<string>(nullable: true),
+                    SecondName = table.Column<string>(nullable: true),
+                    Surname = table.Column<string>(nullable: true),
+                    Age = table.Column<int>(nullable: false),
+                    Address = table.Column<string>(nullable: true),
+                    Balance = table.Column<decimal>(type: "money", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
