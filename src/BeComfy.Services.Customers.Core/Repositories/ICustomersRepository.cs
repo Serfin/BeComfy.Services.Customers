22f1e534cab221fdea0f74c7fd08d0edf2611ae6
serfin <serfin009@gmail.com>
2020-03-07 02:28:51 +0100

    Added missing method in interface

--- src/BeComfy.Services.Customers.Core/Repositories/ICustomersRepository.cs
+++ src/BeComfy.Services.Customers.Core/Repositories/ICustomersRepository.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 using BeComfy.Services.Customers.Core.Entities;
 
@@ -9,6 +10,8 @@ namespace BeComfy.Services.Customers.Core.Repositories
     {
         Task AddAsync(Customer customer);
         Task<IEnumerable<Customer>> BrowseAsync(int pageSize, int page);
+        Task<IEnumerable<Customer>> BrowseAsync(int pageSize, int page, 
+            Expression<Func<Customer, bool>> predicate);
         Task<Customer> GetAsync(Guid id);
         Task UpdateAsync(Customer customer);
         Task DeleteAsync(Guid id);
